866275f20f99d894ae2d14f4b26b5b0506df7725
ziad <66523061+Ziad0dev@users.noreply.github.com>
2026-07-29 14:37:44 +0200

    fixing a really old typo

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -12,7 +12,8 @@ local colors = require("colors")
 --------------------------------------------------------------------------
 ---- MONITORS
 --------------------------------------------------------------------------
-hl.monitor({ output = "DP-2",     mode = "2560x1440@240",  position = "0x0",    scale = 1 })
+hl.monitor({ output = "DP-1", mode = "2560x1440@239.97", position = "0x0", scale = 1 })
+hl.monitor({output = "HDMI-A-1", mode = "1920x0@144", position = "auto-right", scale = 1, transform = 3,})
 -- Fallback for any other monitors
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
 
