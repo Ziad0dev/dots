03ef6f9e6b217025f7ed825cedb6fa17ec8b283c
ziad <66523061+Ziad0dev@users.noreply.github.com>
2026-09-03 11:45:28 +0200

    more human

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -8,32 +8,42 @@ let
 
   packages = [
     "GeForceNOW com.nvidia.geforcenow"
+    "flathub com.github.johnfactotum.Foliate"
+    "flathub com.github.tchx84.Flatseal"
+    "flathub com.usebottles.bottles"
+    "flathub io.github.f3d_app.f3d"
+    "flathub net.meshlab.MeshLab"
+    "flathub org.blender.Blender"
+    "flathub org.gnome.SimpleScan"
+    "flathub org.kde.kdenlive"
+    "flathub org.texstudio.TeXstudio"
   ];
+
+  reconcile = pkgs.writeShellScript "flatpak-reconcile" ''
+    set -eu
+    ${builtins.concatStringsSep "\n" (
+      builtins.attrValues (
+        builtins.mapAttrs (name: url: "flatpak remote-add --user --if-not-exists ${name} ${url}") remotes
+      )
+    )}
+    ${builtins.concatStringsSep "\n" (
+      map (p: "flatpak install --user -y --noninteractive ${p}") packages
+    )}
+  '';
 in
 {
   services.flatpak.enable = true;
 
-  systemd.services.flatpak-managed = {
+  systemd.user.services.flatpak-managed = {
     description = "Reconcile declared flatpak remotes and packages";
-    wantedBy = [ "multi-user.target" ];
-    after = [
-      "network-online.target"
-      "flatpak-system-helper.service"
-    ];
+    wantedBy = [ "default.target" ];
+    after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
     path = [ pkgs.flatpak ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
+      ExecStart = reconcile;
     };
-    script = ''
-      set -eu
-      ${builtins.concatStringsSep "\n" (
-        builtins.attrValues (
-          builtins.mapAttrs (name: url: "flatpak remote-add --if-not-exists ${name} ${url}") remotes
-        )
-      )}
-      ${builtins.concatStringsSep "\n" (map (p: "flatpak install -y --noninteractive ${p}") packages)}
-    '';
   };
 }
