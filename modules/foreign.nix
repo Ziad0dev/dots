23009bc7e9427d5d3ee04f971f7f429ee9cf6d7d
ziad <66523061+Ziad0dev@users.noreply.github.com>
2026-09-03 10:21:49 +0200

    up

---
+++ modules/foreign.nix
@@ -0,0 +1,123 @@
+{ pkgs, ... }:
+
+let
+  runtimeLibs =
+    p: with p; [
+      acl
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      atk
+      bzip2
+      cairo
+      cups
+      curl
+      dbus
+      expat
+      fontconfig
+      freetype
+      fribidi
+      gdk-pixbuf
+      glib
+      gtk3
+      harfbuzz
+      icu
+      libdecor
+      libdrm
+      libgbm
+      libglvnd
+      libice
+      libpulseaudio
+      libsm
+      libsodium
+      libssh
+      libunwind
+      libusb1
+      libuuid
+      libx11
+      libxcb
+      libxcomposite
+      libxcursor
+      libxdamage
+      libxext
+      libxfixes
+      libxi
+      libxinerama
+      libxkbcommon
+      libxml2
+      libxrandr
+      libxrender
+      libxscrnsaver
+      libxslt
+      libxtst
+      nspr
+      nss
+      openssl
+      pango
+      pipewire
+      stdenv.cc.cc
+      systemd
+      util-linux
+      vulkan-loader
+      wayland
+      xz
+      zlib
+      zstd
+    ];
+
+  appimageRun = pkgs.appimage-run.override {
+    extraPkgs = p: [
+      p.libdecor
+      p.pipewire
+      p.libunwind
+    ];
+  };
+
+  fhs = pkgs.buildFHSEnv {
+    name = "fhs";
+    targetPkgs =
+      p:
+      runtimeLibs p
+      ++ (with p; [
+        bashInteractive
+        coreutils
+        diffutils
+        file
+        findutils
+        gawk
+        gnugrep
+        gnused
+        gnutar
+        gzip
+        iana-etc
+        less
+        procps
+        python3
+        which
+        xdg-utils
+      ]);
+    multiPkgs = runtimeLibs;
+    runScript = "bash";
+    profile = "export DOTS_FHS=1";
+  };
+in
+{
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+    package = appimageRun;
+  };
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = runtimeLibs pkgs;
+  };
+
+  services.flatpak.enable = true;
+
+  environment.systemPackages = [
+    fhs
+    pkgs.distrobox
+    pkgs.steam-run
+  ];
+}
