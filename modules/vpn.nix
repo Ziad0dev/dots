f157d44cf0e42182ae6eecdfde763d8a249bd190
ziad <66523061+Ziad0dev@users.noreply.github.com>
2026-09-09 02:06:54 +0200

    up

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -64,7 +64,7 @@ in
       Preferences.WebUI = {
         Address = "*";
         AuthSubnetWhitelistEnabled = true;
-        AuthSubnetWhitelist = lanSubnet;
+        AuthSubnetWhitelist = "192.168.15.0/24";
         LocalHostAuth = false;
         Username = "admin";
         Password_PBKDF2 = "@ByteArray(hm8nwYXMLuaC21xtTlnZgA==:CW/ljNjP7EJ09bUSv6OilUpCO6jNZJo+JGKdnZPISC2VfCGdogqSQdotFulINeRZGOsFcXY6B2qUREd3AXf70A==)";
